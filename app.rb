5f5214dbe105ad3c85ba3e67e6934cf935a6f0c8
Emirhan Kaplan <emskaplann@gmail.com>
2019-10-07 22:17:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,12 +3,14 @@ require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
   get '/' do
+
     erb :index
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @letter = @analyzed_text.most_used_letter.keys 
+    @time = @analyzed_text.most_used_letter[@letter[0]]
     erb :results
   end
 end
